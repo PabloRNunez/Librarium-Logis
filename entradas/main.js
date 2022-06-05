72b4c88d7164f5fd524267e1e8bca57ff7c57629
PabloRNunez <pablorduch1995@gmail.com>
2022-06-05 19:22:25 +0000

    Big update

---
+++ entradas/main.js
@@ -0,0 +1,2 @@
+ScrollReveal().reveal('.nav-main', { delay: 250 });
+ScrollReveal().reveal('.section', { delay: 750 });
---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.nav-main', { delay: 250 });
+ScrollReveal().reveal('.showcase', { delay: 750 });
+ScrollReveal().reveal('.noticiario', { delay: 750 });
+ScrollReveal().reveal('.primerbanner', { delay: 250 });
+ScrollReveal().reveal('.social', { delay: 250 });
