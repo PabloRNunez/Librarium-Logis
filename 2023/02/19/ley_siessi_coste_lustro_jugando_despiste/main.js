bde973ae39efb41a51edbfe8862cc764dc622b06
PabloRNunez <pablorduch1995@gmail.com>
2023-02-19 14:31:21 +0000

    nueva entrada

---
+++ 2023/02/19/ley_siessi_coste_lustro_jugando_despiste/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.nav-main', { delay: 250 });
+ScrollReveal().reveal('h1', { delay: 500 });
+ScrollReveal().reveal('.article', { delay: 750 });
+ScrollReveal().reveal('.social', { delay: 250 });
