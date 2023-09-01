8d1cad69aba5223b64d97c6331b41068353e3bf6
PabloRNunez <pablorduch1995@gmail.com>
2023-09-01 16:00:15 +0100

    nueva entrada

---
+++ 2023/09/01/el_riesgo_de_la_valentia_en_el_auge_de_seacabo/main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.nav-main', { delay: 250 });
+ScrollReveal().reveal('.showcase', { delay: 500 });
+ScrollReveal().reveal('.noticiario', { delay: 750 });
+ScrollReveal().reveal('.primerbanner', { delay: 750 });
+ScrollReveal().reveal('.social', { delay: 1000 });
